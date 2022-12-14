87f5e06bf2b16efc8efbce58b32ca6c2a846dc6d
louisrae <louisrae@settersandspecialists.com>
2022-12-14 14:20:53 +0000

    add functionality for concat yt playlist

--- music.py
+++ music.py
@@ -1,14 +1,21 @@
 # importing time module
 import argparse
+import os
 import random
 import time
 
 import vlc
 from mutagen.mp3 import MP3
+from pytube import YouTube
+
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/opt/homebrew/Cellar/ffmpeg/5.1.2/bin/ffmpeg"
 
 
-def run_song(name):
+from moviepy.editor import AudioFileClip, concatenate_audioclips
+from pytube import Playlist
 
+
+def run_with_rand_position(name):
     track_path = "/Users/louisrae/Documents/dev/music/songs/" + name
     audio = MP3(track_path)
     seconds = int(audio.info.length)
@@ -21,13 +28,23 @@ def run_song(name):
     time.sleep(seconds)
 
 
-parser = argparse.ArgumentParser(
-    prog="Background Music",
-    description="Play A Song At A Random Point",
-)
-parser.add_argument("-f", "--file")
-args = parser.parse_args()
+def download_yt_long_song(yt_link, song_name):
+    pass
 
-run_song(args.file)
 
-#
+def download_yt_playlist(playlist_link, download_folder):
+    for video in Playlist(playlist_link).videos:
+        video.streams.filter(only_audio=True).all()[0].download(download_folder)
+
+
+def concat_audio_files(download_folder, output_file):
+    song_paths = [download_folder + "/" + f for f in os.listdir(download_folder)]
+    clips = [AudioFileClip(c) for c in song_paths]
+    concatenate_audioclips(clips).write_audiofile(output_file)
+
+
+d_folder = "/Users/louisrae/Documents/dev/music/d_songs"
+out_file = "/Users/louisrae/Documents/dev/music/songs/f.mp3"
+concat_audio_files(d_folder, out_file)
+
+# run_with_rand_position()
